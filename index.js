9b5b57ba2a640b12b431c790f050a2bdcf6d0593
Natiom <natioterom@gmail.com>
2023-01-12 20:43:45 -0300

    stats

--- index.js
+++ index.js
@@ -9,15 +9,51 @@ const options = {
     validate : false,
 }
 const pathFile = process.argv[2];
+process.argv.forEach(e =>{
+  if( e == "--stats" ){
+    options.stats = true
+  }
+ if(e == "--validate" ){
+   options.validate = true
+ }
+ })
 
-if(process.argv[3] === '--validate'){
-    options.validate =true
-}
 mdLink.mdLinks(pathFile , options)
-.then((data)=>{
+.then((res =>{
+  if (options.stats && options.validate) {
+    console.log(
+    `${chalk.rgb(0, 128, 128).bold('Total:' + res[0])}
+${chalk.rgb(168, 187, 77, 1).bold('Unique:' + res[1])}
+${chalk.rgb(255, 102, 153).bold('Broken:' + res[2])}`  
+    )
+return }    
+if (options.validate) {
+        if (res.length === 0) {
+         console.log(chalk.red("No se encontraron links"));
+        }
+        let validateLinks = res.map(e => `${chalk.rgb(0, 128, 128)('File:' + e.file)}
+        ${chalk.rgb(168, 187, 77, 1)('Href:' + e.href)}
+        ${chalk.rgb(255, 182, 193, 1)('Text:'  + e.text.substr(0, 40))}
+        ${chalk.rgb(248, 110, 135, 1)('Status:' + e.status)}
+        ${chalk.rgb(255, 102, 153)('ok:' + e.ok)}`
+        );
+  
+        console.log(validateLinks.join('\n'));
+     return }if (options.stats) {
+        console.log(
+        `${chalk.rgb(0, 128, 128).bold('Total:' + res[0])}
+${chalk.rgb(168, 187, 77, 1).bold('Unique:' + res[1])}` 
+        );
+      return }
+  else{
+        let links = res.map(e => `${chalk.rgb(0, 128, 128)('File:' + e.file)}
+        ${chalk.rgb(168, 187, 77, 1)('Href:' + e.href)}
+        ${chalk.rgb(255, 182, 193, 1)('Text:'  + e.text.substr(0, 40))}`
+        );
+        console.log(links.join('\n'));
 
-    console.log(chalk.rgb(255, 102, 153)(`Links:`), data)        
-      })
+      return}
+    }))
 
 
 
--- md-links.js
+++ md-links.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const path = require('path')
 
 //Comprobar que sea un directorio
@@ -20,7 +21,20 @@ const readMd = (pathFile) => {
     });
   });
 };
-
+const filePath = (pathFile) => { 
+  const file =paths(pathFile) === false ? absolute(pathFile) : paths(pathFile)
+  if(statDirectory(file)){
+      const arrayMd = readDir(file);
+      const route = arrayMd.map(fileMd => {
+      const absoluta = absolute(file);
+      const archivo = path.join(`${absoluta}/${fileMd}`)
+      return archivo
+      })
+       return route   
+  }else {
+   return [pathFile]
+  }  
+ };
 // Leer Links
 const getLinks = (pathFile) => {
   return new Promise((resolve, reject)=>{
@@ -28,21 +42,23 @@ const getLinks = (pathFile) => {
     .then((file)=>{
       const links = [];
       const regex = /\[(.+?)\]\((https?:\/\/.+?)\)/g;
-      let match = regex.exec(file);
+      let match = regex.exec(file)
       while (match !== null) {
         links.push({
          href:match[2],
         text: match[1],
          file: pathFile,
-        });
-          match = regex.exec(file);
-          resolve(links)
-        }})
+        });  
+        match = regex.exec(file)
+        }
+        resolve(links)
+      })
         .catch((error)=>{
           reject(error)
         })
         })
       };
+    
      
  // Leer Directorios
  const readDir = (pathFile) =>{
@@ -53,62 +69,134 @@ const getLinks = (pathFile) => {
          const arrayMd =directorio.filter(e => fileMd(e) === '.md');   
            return arrayMd
  };
- const filePath = (pathFile) => { 
- const file =paths(pathFile) === false ? absolute(pathFile) : paths(pathFile)
- if(statDirectory(file)){
-     const arrayMd = readDir(file);
-     const route = arrayMd.map(fileMd => {
-     const absoluta = absolute(file);
-     const archivo = path.join(`${absoluta}/${fileMd}`)
-     return archivo
-     })
-      return route   
- }else {
-  return [pathFile]
- }  
-};
+
 
 //FUNCION PARA VALIDAR LNK CON PETICIONES HTTP
-const validateLinks = (urls) => {
-  return Promise.all(urls.map((arrayLinks) => {
-    return fetch(arrayLinks.href)
-      .then((resolve) => {
-        const objResolve = {
-          ...arrayLinks,
-          status: resolve.status,
-          ok: (resolve.status >= 200) && (resolve.status <= 399) ? "ok" : "fail"
-        }
-        return objResolve;
+const validateLinks = (links) => {
+  return Promise.all(links.map((arrLinks) => {    
+    return fetch(arrLinks.href)
+      .then((res) => {
+        const arrayStatus = {
+          ...arrLinks,
+          status: res.status,
+          ok: (res.ok === true) ? "ok" : "fail"
+        }      
+        return arrayStatus;
+        
       })
       .catch(() => {
         return {
-          ...arrayLinks,
+          ...arrLinks,
           status: "archivo roto",
           ok: "fail"
         }
       })
   })
   )
-};
+ };
+//  const pathFile = process.argv[2];
+//  const file = filePath(pathFile)
+//  file.forEach(e => {getLinks(e)
+//  .then((x)=>{
+//    validateLinks(x)
+//    .then((jeje)=>{
+//     console.log(jeje)
+//    })
+//  })})
 
-const mdLinks = (pathFile ,options) =>  new Promise ((resolve, reject )=> {
-if( options.validate === true){
-    const rutas = filePath(pathFile);
-    const links = Promise.all(rutas.map((file) => getLinks(file)))
-   const validate = links.then((data) => validateLinks(data.flat()));
-      resolve(validate);
+const status = (links)=> { 
+    const total = links.length;
+    const url = links.map((e)=>e.href)
+    const unique = new Set(url).size
+    const  status = links.filter((e)=>e.ok !== 'ok')    
+    const broken = new Set(status).size
+    return  [ total , unique, broken]
+} 
+const mdLinks = (pathFile ,options) => { return new Promise ((resolve , reject)=> {
+  const rutas = filePath(pathFile);
+ if(options.validate === true && options.stats === true){
+    rutas.forEach((file) => getLinks(file)
+    .then((data) => {
+       validateLinks(data)
+       .then((res)=>{
+        resolve(status(res))
+       })
+     })
+  )
+    
+  return } 
+ if(options.validate === true){
+  const file = filePath(pathFile)
+ file.map(e => {getLinks(e)
+ .then((x)=>{
+ validateLinks(x)
+   })
+   })   
+   return}
+if(options.stats === true){
+  rutas.forEach((file) => getLinks(file)
+  .then((data) => {
+     validateLinks(data)
+     .then((res)=>{
+      resolve(status(res))
+     })
+   })
+);        
+ return  }
+else if (options.validate === false && options.stats === false){
+  rutas.forEach((file) =>
+   resolve(getLinks(file))
+   );        
+return }
+ else {reject( new Error('Archivo no encontrado'))}
+}) 
 
-  }if(options.validate === false){
-    const rutas = filePath(pathFile);
-    const links = Promise.all(rutas.map((file => getLinks(file))))
-   resolve(links)
-   
-     }else {
-      reject( new Error ('El archivo no existe'))
-    }
-  });
- 
-  
+  }
+
+// const mdLinks = (pathFile ,options) => { return new Promise ((resolve)=> {
+//   const rutas = filePath(pathFile);
+//   console.log(options)
+// switch (options) {
+//   case options.validate && options.stats:
+//     console.log('uno')
+//     rutas.forEach((file) => getLinks(file)
+//     .then((data) => {
+//        validateLinks(data)
+//        .then((res)=>{
+//         resolve(status(res))
+//        })
+//      })
+//   );              
+//  break;
+// case 'validate' :
+//   rutas.forEach((file) => getLinks(file)
+//      .then((data) => {
+//         resolve(validateLinks(data))
+//       })
+//   );        
+//  break;
+// case options.stats:
+//   rutas.forEach((file) => getLinks(file)
+//   .then((data) => {
+//      validateLinks(data)
+//      .then((res)=>{
+//       resolve(status(res))
+//      })
+//    })
+// );        
+//  break;
+//  case options.validate === false:
+//   rutas.forEach((file) =>
+//    resolve(getLinks(file))
+//    );        
+//  break;
+//  default:
+//   (console.log('Error')) 
+//   }
+// }
+// )}
+
+
   module.exports = {
     statDirectory,
     paths,
